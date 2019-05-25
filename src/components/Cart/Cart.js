64cf2762b7474ae04d5e387c7df76652648f2c50
Jen PC <jenpc100@gmail.com>
2019-05-25 11:45:00 -0400

    paypay button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class Carts extends Component {
                    <Title name="your" title="cart" />
                    <CartColumns />
                    <CartList value={value} />
-                   <CartTotal value={value} />
+                   <CartTotal value={value} history={this.props.history} />
                 </React.Fragment>
               )
             }
